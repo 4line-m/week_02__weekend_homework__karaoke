a97bfd3925d7640455e2be344e37cd8f48a87a56
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-10-08 20:17:53 +0100

    added BarRegister class and passed can_get_bar_register_cash_total test

---
+++ bar_register.rb
@@ -0,0 +1,9 @@
+class BarRegister
+
+  attr_reader(:cash)
+
+  def initialize
+    @cash = 0
+  end
+
+end
---
+++ specs/bar_register_spec.rb
@@ -0,0 +1,15 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../bar_register.rb')
+
+class TestBarRegister < MiniTest::Test
+
+  def setup()
+  @bar_register = BarRegister.new()
+  end
+
+  def test_can_get_bar_register_cash_total()
+    result = @bar_register.cash()
+    assert_equal(0, result)
+  end
+end
